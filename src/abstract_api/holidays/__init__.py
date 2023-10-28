fe5519c45c7eb5caa80da8397dc5ea4f16a95ee7
Ebram Shehata <ebram96@gmail.com>
2023-10-28 05:18:08 +0200

    Add Holidays service

---
+++ src/abstract_api/holidays/__init__.py
@@ -0,0 +1,9 @@
+from typing import Final
+
+from .holidays import Holidays
+from .holidays_response import HolidaysResponse
+
+__all__: Final[list[str]] = [
+    "Holidays",
+    "HolidaysResponse"
+]
---
+++ src/abstract_api/holidays/response_fields.py
@@ -0,0 +1,9 @@
+"""Response fields of Holidays service endpoint."""
+
+
+# The response body does not contain 'holidays' field. This is just to be
+# able to initialize and use the response in a Pythonic way and conform with
+# other parts of the code.
+RESPONSE_FIELDS: frozenset[str] = frozenset({
+    "holidays"
+})
