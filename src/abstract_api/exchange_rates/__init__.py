f26c158b6fca5a77ae249c640bfff7d492244150
Ebram Shehata <ebram96@gmail.com>
2023-10-30 01:50:39 +0200

    Add ExchangeRatesConversionResponse to init file of exchange rates

--- src/abstract_api/exchange_rates/__init__.py
+++ src/abstract_api/exchange_rates/__init__.py
@@ -2,10 +2,12 @@ from typing import Final
 
 from .exchange_rates import ExchangeRates
 from .exchange_rates_conversion_response import ExchangeRatesConversionResponse
+from .historical_exchange_rates_response import HistoricalExchangeRatesResponse
 from .live_exchange_rates_response import LiveExchangeRatesResponse
 
 __all__: Final[list[str]] = [
     "ExchangeRates",
     "ExchangeRatesConversionResponse",
+    "HistoricalExchangeRatesResponse",
     "LiveExchangeRatesResponse"
 ]
