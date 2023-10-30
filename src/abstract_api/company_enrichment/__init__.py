765b219afdce257e8b6ca33e4bea9a84bbd8d121
Ebram Shehata <ebram96@gmail.com>
2023-10-30 03:06:41 +0200

    Add company enrichment service

---
+++ src/abstract_api/company_enrichment/__init__.py
@@ -0,0 +1,9 @@
+from typing import Final
+
+from .company_enrichment import CompanyEnrichment
+from .company_enrichment_response import CompanyEnrichmentResponse
+
+__all__: Final[list[str]] = [
+    "CompanyEnrichment",
+    "CompanyEnrichmentResponse"
+]
