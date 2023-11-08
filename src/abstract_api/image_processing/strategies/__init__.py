72871d9647c0879d28c8ed910b33168e3ad1ba81
Ebram Shehata <ebram96@gmail.com>
2023-11-09 01:57:01 +0200

    Add Auto strategy class

--- src/abstract_api/image_processing/strategies/__init__.py
+++ src/abstract_api/image_processing/strategies/__init__.py
@@ -1,11 +1,13 @@
 from typing import Final
 
+from .auto import Auto
 from .base_strategy import BaseStrategy
 from .exact import Exact
 from .landscape import Landscape
 from .portrait import Portrait
 
 __all__: Final[list[str]] = [
+    "Auto",
     "BaseStrategy",
     "Exact",
     "Landscape",
