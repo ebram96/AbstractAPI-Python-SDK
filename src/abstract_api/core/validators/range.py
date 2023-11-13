2a37d8919604d8d0f627269604a075e3d79df875
Ebram Shehata <ebram96@gmail.com>
2023-11-13 14:39:25 +0200

    Add validation for avatars service parameters

---
+++ src/abstract_api/core/validators/range.py
@@ -0,0 +1,29 @@
+from typing import Any
+
+from abstract_api.core.exceptions import ClientRequestError
+
+
+def between(
+    param: str,
+    value: Any,
+    start: int | float,
+    end: int | float
+) -> None:
+    """Validate a value to be in a range (inclusive).
+
+    Args:
+        param: Parameter name.
+        value: Parameter value.
+        start: Beginning of the range.
+        end: End of the range.
+
+    Raises:
+        ClientRequestError if the given value is not in the range (inclusive).
+    """
+    if value is None:
+        return
+
+    if not (start <= value <= end):
+        raise ClientRequestError(
+            f"'{param}' must be in range from {start} to {end} (inclusive)"
+        )
