400b54c7905dd619f63b37e19c438b05e9b1cc5a
Ebram Shehata <ebram96@gmail.com>
2023-12-30 00:54:18 +0200

    Add VAT tests

---
+++ tests/vat/test_vat.py
@@ -0,0 +1,107 @@
+import pytest
+
+from abstract_api import VAT
+from abstract_api.core.validators import numerical
+from abstract_api.vat import (
+    VATCalculationResponse,
+    VATCategoriesResponse,
+    VATValidationResponse
+)
+
+
+class TestVAT:
+    @pytest.fixture
+    def service(self):
+        return VAT(api_key="no-api-key")
+
+    def test_check(
+        self,
+        service,
+        vat_validation_sample,
+        base_url,
+        mocker,
+        requests_mock
+    ):
+        # Given
+        vat_number = vat_validation_sample["vat_number"]
+        action = "validate"
+        url = base_url.format(subdomain=VAT._subdomain)
+        requests_mock.get(url + action, json=vat_validation_sample)
+        mocked__service_request = mocker.patch.object(
+            service, "_service_request", wraps=service._service_request
+        )
+
+        # When
+        response = service.check(vat_number)
+
+        # Then
+        assert isinstance(response, VATValidationResponse)
+        mocked__service_request.assert_called_once_with(
+            _response_class=VATValidationResponse,
+            _action=action,
+            vat_number=vat_number
+        )
+
+    def test_calculate(
+        self,
+        service,
+        vat_calculation_sample,
+        base_url,
+        mocker,
+        requests_mock
+    ):
+        # Given
+        amount = float(vat_calculation_sample["amount_excluding_vat"])
+        country_code = vat_calculation_sample["country"]["code"]
+        action = "calculate"
+        url = base_url.format(subdomain=VAT._subdomain)
+        requests_mock.get(url + action, json=vat_calculation_sample)
+        mocked__service_request = mocker.patch.object(
+            service, "_service_request", wraps=service._service_request
+        )
+        mocked_greater_or_equal = mocker.patch.object(
+            numerical, "greater_or_equal", wraps=numerical.greater_or_equal
+        )
+
+        # When
+        response = service.calculate(amount, country_code)
+
+        # Then
+        assert isinstance(response, VATCalculationResponse)
+        mocked__service_request.assert_called_once_with(
+            _response_class=VATCalculationResponse,
+            _action=action,
+            amount=amount,
+            country_code=country_code,
+            is_vat_incl=None,
+            vat_category=None
+        )
+        mocked_greater_or_equal.assert_called_once_with("amount", amount, 0)
+
+    def test_lookup(
+        self,
+        service,
+        vat_categories_sample,
+        base_url,
+        mocker,
+        requests_mock
+    ):
+        # Given
+        country_code = vat_categories_sample[0]["country_code"]
+        action = "categories"
+        url = base_url.format(subdomain=VAT._subdomain)
+        requests_mock.get(url + action, json=vat_categories_sample)
+        mocked__service_request = mocker.patch.object(
+            service, "_service_request", wraps=service._service_request
+        )
+
+        # When
+        response = service.categories(country_code)
+
+        # Then
+        assert isinstance(response, VATCategoriesResponse)
+        mocked__service_request.assert_called_once_with(
+            _response_class=VATCategoriesResponse,
+            _action=action,
+            country_code=country_code,
+        )
